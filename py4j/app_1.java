4a1d33f555b6aa898ff5d0b95f4ff959eaba836e
Sahil Goyal <goyalsahil308@gmail.com>
2022-08-25 11:01:17 +0530

    minor changes

--- py4j/app_1.java
+++ py4j/app_1.java
@@ -4,5 +4,14 @@ public interface app_1
   /** This interface contain all the methods we want to call from java
       */
 {
-    
+ You have  matrix m [1000][1000] = [ [num py array]  , [], [], [],
+              [], [], [], [][],
+               [], [], [], [],[]
+               ...
+               ...] 
+
+               input matrix n[5][5] = [[], [], []
+                                       [] , []
+                                       ....]
+          You should print time taken to find it and it should consider all conditions 
 }
